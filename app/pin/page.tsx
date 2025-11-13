83b82404a19d974ad70a54fd6e23927da55728e6
gawstxn <naruenat.privatez@gmail.com>
2025-11-13 21:27:36 +0700

    style: margin pin page

--- app/pin/page.tsx
+++ app/pin/page.tsx
@@ -3,7 +3,7 @@ import { PINForm } from "@/components/pin-form"
 export default function PinPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="w-full max-w-sm">
+      <div className="w-full max-w-sm mb-10">
         <PINForm />
       </div>
     </div>
