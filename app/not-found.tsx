ee5ec9625c06113cbdb98b41f779c55e4ce3b6fa
gawstxn <naruenat.privatez@gmail.com>
2025-11-13 17:39:28 +0700

    fix: redirect unknow path

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,22 +1,9 @@
 // app/not-found.tsx
-import { verifyPinToken } from "@/lib/auth"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
 export default async function NotFoundPage() {
   const token = (await cookies()).get("token")?.value
-
   if (!token) redirect("/pin")
-
-  try {
-    const decoded = verifyPinToken(token)
-    redirect("/dashboard")
-  } catch (err) {
-    // token invalid → ไป /pin
-    redirect("/pin")
-  }
-
-  return (
-    <div>test</div>
-  )
+  redirect("/dashboard")
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
-import { verifyPinToken } from "@/lib/auth"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
 export default async function HomePage() {
   const token = (await cookies()).get("token")?.value
-
   if (!token) redirect("/pin")
-
-  try {
-    const decoded = verifyPinToken(token)
-  } catch (err) {
-    redirect("/pin")
-  }
-
   redirect("/dashboard")
 }
