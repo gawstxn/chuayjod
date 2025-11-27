fedc3ec6b43b99bb983d9d8aeb708ba33d76c237
gawstxn <naruenat.privatez@gmail.com>
2025-11-27 12:55:22 +0700

    optimize api

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,18 @@
-import { LucideIcon } from "lucide-react"
+import { LucideIcon } from "lucide-react";
 
 export interface Route {
   label: string
   path: string
   icon: LucideIcon
+}
+
+export interface TransactionItem {
+  [key: string]: string | number | null; 
+}
+
+export interface PaginatedData {
+  items: TransactionItem[];
+  page: number;
+  pageSize: number;
+  total?: number;
 }
